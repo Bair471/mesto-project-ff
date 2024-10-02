29ec9c6b31bc43577022949fd881dc9fe1bdf728
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-10-02 19:23:09 +0800

    Commit1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,12 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const profile = document.querySelector(".profile");
+const addButton = profile.querySelector(".profile__add-button");
+
+const popupEdit = document.querySelector(".popup_type_edit");
+const popupAdd = document.querySelector(".popup_type_new-card");
+const popupImage = document.querySelector(".popup popup_type_image");
+
