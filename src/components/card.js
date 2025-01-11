b29ffd3bbf93ec514f9640df9b25f67439fec706
Bair471 <bair.shirnen@gmail.com>
2025-01-12 02:25:24 +0800

    Commitfix7

--- src/components/card.js
+++ src/components/card.js
@@ -44,7 +44,7 @@ function createCard(card, onDelete, handleOpenImage) {
           likeCounter.textContent = updatedCard.likes.length;
         })
         .catch(err => {
-          console.log('Ошибка при удалении лайка:', err);
+          throw new Error('Ошибка при удалении лайка: ' + err.message);
         });
     } else {
       // Если лайк не поставлен, добавим его
@@ -54,7 +54,7 @@ function createCard(card, onDelete, handleOpenImage) {
           likeCounter.textContent = updatedCard.likes.length;
         })
         .catch(err => {
-          console.log('Ошибка при добавлении лайка:', err);
+          throw new Error('Ошибка при удалении лайка: ' + err.message);
         });
     }
   });
